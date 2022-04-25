ce1b9f81775c14fd860a8a3257fa24043e10ebc1
ujjwalgupta94 <80671501+ujjwalgupta94@users.noreply.github.com>
2022-04-25 11:19:59 +0530

    convert block baseFeePerGas to hex

--- src/utils/eth_block_to_matic_block.ts
+++ src/utils/eth_block_to_matic_block.ts
@@ -4,6 +4,8 @@ import { providers } from "ethers";
 export const ethBlockToMaticBlock = (block: providers.Block) => {
     block.gasUsed = block.gasUsed.toNumber() as any;
     block.gasLimit = block.gasLimit.toNumber() as any;
-
+    if ((block as any).baseFeePerGas) {
+        (block as any).baseFeePerGas = block.baseFeePerGas.toHexString();
+    }
     return block as any as IBlock;
 };
